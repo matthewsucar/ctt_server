0bcbe9dc1bfd4658e120fde649f4338a33ee069c
Ben Matthews <matthews@ucar.edu>
2026-03-26 16:54:17 -0600

    tweak build notes

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cargo install cargo-generate-rpm
-RUSTC_BOOTSTRAP=1 cargo build --release
+#RUSTC_BOOTSTRAP to trick the stable compiler into working
+#if using nightly you don't need that and shouldn't use it
+RUSTC_BOOTSTRAP=1 cargo build --features slack,pbs --release
 cargo generate-rpm --auto-req disabled
